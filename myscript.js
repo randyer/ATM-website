1f823f09b1c7d50df76ed8b3cd7ba112315266e8
randyer <62490854+randyer@users.noreply.github.com>
2022-12-30 11:59:29 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,33 @@
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', function() {
+  modal.showModal();
+})
+
+closeModal.addEventListener('click', function() {
+  modal.close();
+})
+
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if(visibility === "false"){
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded',true);
+    }
+    else if(visibility === "true"){
+        primaryNav.setAttribute('data-visible',false);
+        navToggle.setAttribute('aria-expanded',false);
+    }
+})
+
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}; 
